b9e8b186fc44e5d82388da484db54629c68593c8
Albert <amuluowin@163.com>
2021-08-04 17:54:37 +0800

    fix return

--- src/Stores/IProxyStore.php
+++ src/Stores/IProxyStore.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Rabbit\Spider\Stores;
 
-use Generator;
-
 interface IProxyStore
 {
     public function save(array $data, bool $onlyUpdate = false): void;
 
     public function delete(array $query): void;
 
-    public function getUse(array $query): Generator;
+    public function getUse(array $query): array;
 }

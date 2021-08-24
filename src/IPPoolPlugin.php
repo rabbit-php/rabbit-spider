8b1daf56f6158396d228ecd01e4a4c63d9eb0206
Albert <amuluowin@163.com>
2021-08-24 21:20:26 +0800

    use spl channel

--- src/IPPoolPlugin.php
+++ src/IPPoolPlugin.php
@@ -4,19 +4,52 @@ declare(strict_types=1);
 
 namespace Rabbit\Spider;
 
+use Rabbit\Base\Core\SplChannel;
 use Rabbit\Base\Helper\ArrayHelper;
 use Rabbit\Spider\Register\AbstractRegister;
+use Swoole\Coroutine;
 
 abstract class IPPoolPlugin extends AbstractProxyPlugin
 {
     protected ?string $tunnel = null;
     protected AbstractRegister $regist;
 
+    protected SplChannel $channel;
+    protected int $size = 1000;
+    protected int $maxSize = 20000;
+    protected array $runItems = [];
+    protected int $cid = 0;
+    protected string $workerName;
+
     public function init(): void
     {
         parent::init();
-        $this->tunnel = ArrayHelper::getValue($this->config, 'tunnel');
+        [
+            $this->tunnel,
+            $this->size,
+            $this->maxSize
+        ] = ArrayHelper::getValueByArray($this->config, ['tunnel', 'size', 'maxSize'], [$this->tunnel, $this->size, $this->maxSize]);
+        $this->channel = new SplChannel();
         $this->regist = getDI('register')->get($this->taskName);
         $this->regist->regist();
+        $this->workerName = $this->regist->getMsg();
+    }
+
+    public function check(): bool
+    {
+        if (count($this->runItems) > $this->maxSize) {
+            $this->cid = Coroutine::getCid();
+            Coroutine::yield();
+        }
+        return true;
+    }
+
+    public function start(): void
+    {
+        if (count($this->runItems) <= $this->maxSize && $this->cid > 0) {
+            $cid = $this->cid;
+            $this->cid = 0;
+            Coroutine::resume($cid);
+        }
     }
 }
